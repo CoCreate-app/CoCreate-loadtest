1cd1ca741f087f9bec1ea977b38f72b411c04deb
Ramazan <78518491+ramazan821@users.noreply.github.com>
2021-03-17 07:01:24 +0900

    updated http_post.js

--- http_post.js
+++ http_post.js
@@ -1,6 +1,12 @@
 import { sleep } from 'k6';
 import http from 'k6/http';
 
+export let options = {
+    vus: 100,
+    duration: '20s',
+    rps: 500
+};
+
 
 export function setup() {
     var url = 'http://test.k6.io/login.php';
