3a72205a3c16e70b817374a6f52f06227e7092ee
Ramazan <78518491+ramazan821@users.noreply.github.com>
2021-03-17 06:59:18 +0900

    updated http_get.js

--- http_get.js
+++ http_get.js
@@ -1,16 +1,12 @@
-import { sleep } from 'k6';
 import http from 'k6/http';
 
+export let options = {
+    vus: 100,
+    duration: '20s',
+    rps: 500
+};
 
-export function setup() {
-    let res = http.get('https://httpbin.org/get');
-    return { data: res.json() };
-}
 
-export function teardown(data) {
-    console.log(JSON.stringify(data));
-}
-
-export default function(data) {
-    sleep(1);
+export default function() {
+    http.get('http://test.k6.io/');
 }
